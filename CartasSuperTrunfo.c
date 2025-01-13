943480b45e8481700b2c0586d34a08244a14c043
deboradevsouza <59833039+deboradevsouza@users.noreply.github.com>
2025-01-13 21:36:37 +0000

    Cadastro de cartas - Nível novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,11 +8,12 @@
 
 int main() {
 
-    // Inseri o título do desafio - Iniciante:
+    // Inseri o título do desafio - Nível novato:
+
     printf("Desafio de Cartas Super Trunfo - Países \n");
 
     // Defini o tipo e o nome das variáveis:
-    char estado[20];
+    char estado[50];
     char codigo[20];
     char nome[50];
     int populacao;
@@ -22,16 +23,36 @@ int main() {
 
     // Defini o que sairá em cada printf e scanf:
 
-    printf("Escreva o nome do Estado: \n");
-    scanf("%c", estado);
+    printf("Escreva a sigla do Estado (sem utilizar espaço): \n");
+    scanf("%s", &estado);
+    
+    printf("Digite o código da carta: \n");
+    scanf("%s", &codigo);
 
+    printf("Escreva o nome da cidade (sem utilizar espaço): \n");
+    scanf("%s", &nome);
 
+    printf("Digite a área da cidade: \n");
+    scanf("%f", &area);
 
+    printf("Digite o PIB da cidade: \n");
+    scanf("%f", &pib);
 
+    printf("Digite o número de pontos turísticos da cidade: \n");
+    scanf("%d", &pontosturisticos);
 
+    // Defini o retorno ao usuário após o cadastro da carta feita pelo mesmo. 
 
+    printf("Você cadastrou esta carta: \n");
+    
+    printf("Código da carta criada: %s \n", codigo);
+    printf("Estado: %s - Cidade: %s \n", estado, nome);
+    printf("População: %f \n", populacao);
+    printf("Tamanho da área: %f km² \n", area);
+    printf("PIB: %f \n", pib);
+    printf("Quantidade de pontos turísticos: %d \n", pontosturisticos);
 
-
+        
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
